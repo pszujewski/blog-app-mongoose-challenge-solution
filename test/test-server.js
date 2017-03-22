1c22d007c735da62acf858f6f67498b59d3ecd57
Alvin Law <alvinlaw@gmail.com>
2017-03-22 09:57:19 -0700

    Start test-server.js with describe block -ally

--- test/test-server.js
+++ test/test-server.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+
 const seedData = require('../seed-data.json');
 console.log(seedData);
 
@@ -11,5 +13,36 @@ const {TEST_DATABASE_URL} = require('../config');
 const should = chai.should();
 
 chai.use(chaiHttp);
+
+function seedDatabase(){
+    return BlogPost.insertMany(seedData);
+}
+
+function tearDownDb(){
+    console.warn('Deleting Database!!');
+    return mongoose.connection.dropDatabase();
+}
+
+describe('Blog API Resource', function(){
+    before(function (){
+        return runServer(TEST_DATABASE_URL);
+    });
+    beforeEach(function (){
+        return seedDatabase(seedData);
+    });
+    after(function (){
+        return closeServer();
+    });
+    afterEach(function (){
+        return tearDownDb();
+    });
+
+    describe('Get Endpoint', function(){
+        it('Should Return All Existing Blog Post'){
+            
+        }:
+    });
+});
+
 
 
